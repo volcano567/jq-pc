35876e4d229cdbaf7eb934f54537c51228f4750d
mine <953425754@qq.com>
2020-10-15 19:45:52 +0800

    10.15

--- new/js/common.js
+++ new/js/common.js
@@ -14,3 +14,16 @@ $(".listNav ul li").each(function(){
         $(this).removeClass("listactive");
     }
 });
+
+function GetRequest() {
+    var url = location.search; //获取url中"?"符后的字串
+    var theRequest = new Object();
+     if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        strs = str.split("&");
+        for (var i = 0; i < strs.length; i++) {
+            theRequest[strs[i].split("=")[0]] = decodeURIComponent(strs[i].split("=")[1]);
+        }
+    }
+    return theRequest;
+}
